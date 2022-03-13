6a7d655cdd45ebf3588c9f2cf4d865d3ea81aa77
Luiz Stevanatto <luizstevanatto@hotmail.com>
2022-03-13 16:27:00 -0300

    CreateMovie  Migration

---
+++ backend/src/database/migrations/1647198754949-CreateMovie.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateMovie1647198754949 implements MigrationInterface {
+    private table = new Table({
+        name: "movies",
+        columns: [
+            {
+                name: "_id",
+                type: "uuid",
+                isPrimary: true,
+            },
+            {
+                name: "name",
+                type: "varchar",
+            },
+            {
+                name: "gender",
+                type: "varchar",
+            },
+            {
+                name: "duration",
+                type: "varchar",
+            },
+            {
+                name: "classification",
+                type: "varchar",
+            },
+            {
+                name: "launch",
+                type: "varchar",
+            },
+            {
+                name: "synopsis",
+                type: "varchar",
+            },
+        ],
+    });
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(this.table);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable(this.table);
+    }
+}
---
+++ backend/src/database/migrations/1647198788311-CreateSession.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateSession1647198788311 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
